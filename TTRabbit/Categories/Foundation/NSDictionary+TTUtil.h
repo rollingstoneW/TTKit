d4abe2466b41e2bfdef005ff3134a7946163c407
weizhenning <weizhenning@zuoyebang.com>
2021-03-24 09:41:03 +0800

    NSDictionary添加通过keyPath取指定类型value的方法

--- TTRabbit/Categories/Foundation/NSDictionary+TTUtil.h
+++ TTRabbit/Categories/Foundation/NSDictionary+TTUtil.h
@@ -13,12 +13,116 @@ typedef id _Nullable (^TTMergingConflict)(id newValue, id currentValue);
 
 @interface NSDictionary (TTUtil)
 
-- (nullable NSDictionary *)tt_dictionaryValueForKey:(NSString *)key defaultValue:(nullable NSDictionary *)def;
+/**
+ 根据keyPath取到字典类型的数据，如果为空则返回默认值def
+ */
+- (nullable NSDictionary *)tt_dictionaryValueForKeyPath:(NSString *)keyPath default:(nullable NSDictionary *)def;
 
-- (nullable NSArray *)tt_arrayValueForKey:(NSString *)key defaultValue:(nullable NSArray *)def;
+/**
+ 根据keyPath取到数组类型的数据，如果为空则返回默认值def
+ */
+- (nullable NSArray *)tt_arrayValueForKeyPath:(NSString *)keyPath default:(nullable NSArray *)def;
 
+/**
+ 根据keyPath取到NSNumber类型的数据，如果为空则返回默认值def
+ */
+- (nullable NSNumber *)tt_numberValueForKeyPath:(NSString *)keyPath default:(nullable NSNumber *)def;
+
+/**
+ 根据keyPath取到NSString类型的数据，如果为空则返回默认值def
+ */
+- (nullable NSString *)tt_stringValueForKeyPath:(NSString *)keyPath default:(nullable NSString *)def;
+
+/**
+ 根据keyPath取到bool类型的数据，如果为空则返回默认值def
+ */
+- (BOOL)tt_boolValueForKeyPath:(NSString *)keyPath default:(BOOL)def;
+
+/**
+ 根据char取到NSNumber类型的数据，如果为空则返回默认值def
+ */
+- (char)tt_charValueForKeyPath:(NSString *)keyPath default:(char)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (unsigned char)tt_unsignedCharValueForKeyPath:(NSString *)keyPath default:(unsigned char)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (short)tt_shortValueForKeyPath:(NSString *)keyPath default:(short)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (unsigned short)tt_unsignedShortValueForKeyPath:(NSString *)keyPath default:(unsigned short)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (int)tt_intValueForKeyPath:(NSString *)keyPath default:(int)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (unsigned int)tt_unsignedIntValueForKeyPath:(NSString *)keyPath default:(unsigned int)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (long)tt_longValueForKeyPath:(NSString *)keyPath default:(long)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (unsigned long)tt_unsignedLongValueForKeyPath:(NSString *)keyPath default:(unsigned long)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (long long)tt_longLongValueForKeyPath:(NSString *)keyPath default:(long long)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (unsigned long long)tt_unsignedLongLongValueForKeyPath:(NSString *)keyPath default:(unsigned long long)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (float)tt_floatValueForKeyPath:(NSString *)keyPath default:(float)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (double)tt_doubleValueForKeyPath:(NSString *)keyPath default:(double)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (NSInteger)tt_integerValueForKeyPath:(NSString *)keyPath default:(NSInteger)def;
+
+/**
+ 根据keyPath取到unsignedChar类型的数据，如果为空则返回默认值def
+ */
+- (NSUInteger)tt_unsignedIntegerValueForKeyPath:(NSString *)keyPath default:(NSUInteger)def;
+
+/**
+ 合并两个字典，如果key重复，则用other里的value覆盖原value
+ */
 - (NSDictionary *)tt_dictionaryByMergingOtherUsingNew:(NSDictionary *)other;
+
+/**
+ 合并两个字典
+ 如果key重复，若usingStrict为YES，则不添加other里key对应的value，否则添加other里key对应的value，若原value为数组，则直接添加进数组，否则原value变为包含原value和新value的数组。
+ */
 - (NSDictionary *)tt_dictionaryByMergingOtherCombined:(NSDictionary *)other usingStrict:(BOOL)usingStrict;
+
+/**
+ 合并两个字典
+ conflict：如何解决key重复的情况，如果返回newValue，则使用newValue，否则使用原value
+ */
 - (NSDictionary *)tt_dictionaryByMergingOther:(NSDictionary *)other conflict:(TTMergingConflict)conflict;
 
 @end
--- TTRabbit/Categories/Foundation/NSDictionary+TTUtil.m
+++ TTRabbit/Categories/Foundation/NSDictionary+TTUtil.m
@@ -10,27 +10,156 @@
 
 @implementation NSDictionary (TTUtil)
 
-- (nullable NSDictionary *)tt_dictionaryValueForKey:(NSString *)key defaultValue:(nullable NSDictionary *)def {
-    if (!key.length) {
-        return def;
+/// Get a number value from 'id'.
+static NSNumber *TTNSNumberFromString(NSString *value) {
+    static NSCharacterSet *dot;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dot = [NSCharacterSet characterSetWithRange:NSMakeRange('.', 1)];
+    });
+    NSString *lower = ((NSString *)value).lowercaseString;
+    if ([lower isEqualToString:@"true"] || [lower isEqualToString:@"yes"]) return @(YES);
+    if ([lower isEqualToString:@"false"] || [lower isEqualToString:@"no"]) return @(NO);
+    if ([lower isEqualToString:@"nil"] || [lower isEqualToString:@"null"]) return nil;
+    if ([(NSString *)value rangeOfCharacterFromSet:dot].location != NSNotFound) {
+        return @(((NSString *)value).doubleValue);
+    } else {
+        return @(((NSString *)value).longLongValue);
     }
-    id value = [self objectForKey:key];
-    if ([value isKindOfClass:[NSDictionary class]]) {
-        return value;
-    }
-    return def;
 }
 
-- (nullable NSArray *)tt_arrayValueForKey:(NSString *)key defaultValue:(nullable NSArray *)def {
-    if (!key.length) {
+- (NSDictionary *)tt_dictionaryValueForKeyPath:(NSString *)keyPath default:(NSDictionary *)def {
+    if (!keyPath.length) {
         return def;
     }
-    id value = [self objectForKey:key];
-    if ([value isKindOfClass:[NSArray class]]) {
-        return value;
+    NSArray *keys = [keyPath componentsSeparatedByString:@"."];
+    NSDictionary *curDict = self;
+    for (NSInteger i = 0; i < keys.count; i++) {
+        id value = [curDict objectForKey:keys[i]];
+        if (![value isKindOfClass:[NSDictionary class]]) {
+            return def;
+        }
+        curDict = value;
     }
-    return def;
+    return curDict;
 }
+
+#define TTRETURN_OBJECTVALUE(_type_, ...) \
+if (!keyPath.length) { \
+    return def; \
+} \
+NSArray *keys = [keyPath componentsSeparatedByString:@"."]; \
+if (keys.count == 1) { \
+    id value = [self objectForKey:keys.firstObject]; \
+    if ([value isKindOfClass:[_type_ class]]) { return value; } \
+    __VA_ARGS__; \
+    return def;\
+} \
+NSArray *newKeys = [keys subarrayWithRange:NSMakeRange(0, keys.count - 1)]; \
+NSDictionary *preDict = [self tt_dictionaryValueForKeyPath:[newKeys componentsJoinedByString:@"."] default:nil]; \
+if (!preDict) return nil; \
+id value = [preDict objectForKey:keys.lastObject]; \
+if ([value isKindOfClass:[_type_ class]]) { return value; } \
+__VA_ARGS__; \
+return def;\
+
+- (nullable NSArray *)tt_arrayValueForKeyPath:(NSString *)keyPath default:(NSArray *)def {
+    TTRETURN_OBJECTVALUE(NSArray, nil)
+}
+
+- (nullable NSNumber *)tt_numberValueForKeyPath:(NSString *)keyPath default:(nullable NSNumber *)def {
+    TTRETURN_OBJECTVALUE(NSNumber, if ([value isKindOfClass:[NSString class]]) {
+        return TTNSNumberFromString(value);
+    })
+}
+
+- (nullable NSString *)tt_stringValueForKeyPath:(NSString *)keyPath default:(nullable NSString *)def {
+    TTRETURN_OBJECTVALUE(NSString, if ([value isKindOfClass:[NSNumber class]]) {
+        return [value description];
+    })
+}
+
+#undef TTRETURN_OBJECTVALUE
+
+#define TTRETURN_NUMBERVALUE(_type_) \
+if (!keyPath.length) { \
+    return def; \
+} \
+NSArray *keys = [keyPath componentsSeparatedByString:@"."]; \
+if (keys.count == 1) { \
+    id value = [self objectForKey:keys.firstObject]; \
+    if ([value isKindOfClass:[NSNumber class]]) return [value _type_]; \
+    if ([value isKindOfClass:[NSString class]]) return TTNSNumberFromString(value)._type_; \
+    return def; \
+} \
+NSArray *newKeys = [keys subarrayWithRange:NSMakeRange(0, keys.count - 1)]; \
+NSDictionary *preDict = [self tt_dictionaryValueForKeyPath:[newKeys componentsJoinedByString:@"."] default:nil]; \
+id value = [preDict objectForKey:keys.lastObject]; \
+if ([value isKindOfClass:[NSNumber class]]) return [value doubleValue]; \
+if ([value isKindOfClass:[NSString class]]) return TTNSNumberFromString(value).doubleValue; \
+return def;
+
+- (BOOL)tt_boolValueForKeyPath:(NSString *)keyPath default:(BOOL)def {
+    TTRETURN_NUMBERVALUE(boolValue)
+}
+
+- (char)tt_charValueForKeyPath:(NSString *)keyPath default:(char)def {
+    TTRETURN_NUMBERVALUE(charValue)
+}
+
+- (unsigned char)tt_unsignedCharValueForKeyPath:(NSString *)keyPath default:(unsigned char)def {
+    TTRETURN_NUMBERVALUE(unsignedCharValue)
+}
+
+- (short)tt_shortValueForKeyPath:(NSString *)keyPath default:(short)def {
+    TTRETURN_NUMBERVALUE(shortValue)
+}
+
+- (unsigned short)tt_unsignedShortValueForKeyPath:(NSString *)keyPath default:(unsigned short)def {
+    TTRETURN_NUMBERVALUE(unsignedShortValue)
+}
+
+- (int)tt_intValueForKeyPath:(NSString *)keyPath default:(int)def {
+    TTRETURN_NUMBERVALUE(intValue)
+}
+
+- (unsigned int)tt_unsignedIntValueForKeyPath:(NSString *)keyPath default:(unsigned int)def {
+    TTRETURN_NUMBERVALUE(unsignedIntValue)
+}
+
+- (long)tt_longValueForKeyPath:(NSString *)keyPath default:(long)def {
+    TTRETURN_NUMBERVALUE(longValue)
+}
+
+- (unsigned long)tt_unsignedLongValueForKeyPath:(NSString *)keyPath default:(unsigned long)def {
+    TTRETURN_NUMBERVALUE(unsignedLongValue)
+}
+
+- (long long)tt_longLongValueForKeyPath:(NSString *)keyPath default:(long long)def {
+    TTRETURN_NUMBERVALUE(longLongValue)
+}
+
+- (unsigned long long)tt_unsignedLongLongValueForKeyPath:(NSString *)keyPath default:(unsigned long long)def {
+    TTRETURN_NUMBERVALUE(unsignedLongLongValue)
+}
+
+- (float)tt_floatValueForKeyPath:(NSString *)keyPath default:(float)def {
+    TTRETURN_NUMBERVALUE(floatValue)
+}
+
+- (double)tt_doubleValueForKeyPath:(NSString *)keyPath default:(double)def {
+    TTRETURN_NUMBERVALUE(doubleValue)
+}
+
+- (NSInteger)tt_integerValueForKeyPath:(NSString *)keyPath default:(NSInteger)def {
+    TTRETURN_NUMBERVALUE(integerValue)
+}
+
+- (NSUInteger)tt_unsignedIntegerValueForKeyPath:(NSString *)keyPath default:(NSUInteger)def {
+    TTRETURN_NUMBERVALUE(unsignedIntegerValue)
+}
+
+#undef TTRETURN_NUMBERVALUE
 
 - (NSDictionary *)tt_dictionaryByMergingOtherUsingNew:(NSDictionary *)other {
     return [self tt_dictionaryByMergingOther:other conflict:^id _Nullable(id  _Nonnull newValue, id  _Nonnull currentValue) {
